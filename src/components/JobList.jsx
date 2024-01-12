c002617824bb2bb1acd69f48a2ebe5d8b6d2c8f8
Sumit Agarwal <agawralsumit334ju@gmail.com>
2024-01-12 15:52:30 +0530

    Error Resolved

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 
-
-
-const JobList=({onSelectJob, jobs})=>{
-    return (    
+const JobList = ({ onSelectJob, jobs }) => {
+    return (
         <div>
             <h2 className="mt-3 mb-4">Job Listings</h2>
             <ul className="list-group">
                 {jobs && jobs.map((job) => (
                     <li className="list-group-item" key={job._id}>
-                        <a href="#" onClick={() => onSelectJob(job)} className="text-decoration-none">
-                        {job.title} - {job.company}
-                        </a>
+                        {/* Use a button element instead of an anchor */}
+                        <button
+                            type="button"
+                            className="btn btn-link text-decoration-none"
+                            onClick={() => onSelectJob(job)}
+                        >
+                            {job.title} - {job.company}
+                        </button>
                     </li>
                 ))}
             </ul>
         </div>
-    )
-}
+    );
+};
 
 export default JobList;
